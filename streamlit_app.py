f4e78ccc47f1e50b4f5bdbc5b0a69bd9c763c4c4
afennell-tech <afennell@ucsd.edu>
2021-10-27 13:37:07 -0700

    added slider and using subset of all data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import geopandas as gpd
 
+FIRE_DATA = 'data/subset.pkl'
 st.write('Our dashboard starts here!')
 # st.slider('ex', 1992, 2018,)
 
@@ -17,22 +18,24 @@ def load_boundary_data():
     div = gpd.read_file('data/division')
     return state, county, div
 
-@st.cache(allow_output_mutation=True)
+@st.cache
 def load_fire_data():
-    fire_df = pd.read_pickle('data/clean_fpa_fod.pkl')
+    fire_df = pd.read_pickle(FIRE_DATA)
     return fire_df
 
 # load boundary data for map plots on first run
 state_df, county_df, div_df = load_boundary_data()
 # load fire related dataset on first run
-fire_df = load_fire_data().copy()
+# fire_df = load_fire_data()
+fire_df = load_fire_data()
 
-choice = st.selectbox('ex', [0,1])
-choice_dict = {
-    0: fire_df.index.year[0],
-    1: fire_df.index.year[1] 
-}
-st.write(choice_dict[choice])
+state = st.selectbox("State", fire_df.STATE.values.unique(), index=0)
+st.write(f'state:{state}')
+min_val = 0
+max_val = int(fire_df.FIRE_SIZE.max())
+fire_size = st.slider(label="Range of Fire Size", value=(min_val, max_val))
+st.write(f'fire size: {fire_size}')
+# st.write(choice_dict[choice])
 # x = st.slider('Year', fpa_fod.index.year[0], fpa_fod.index.year[-1])
 # st.write('x={x}')
 # val = st.date_input('year of fire', fpa_fod.index.year[0])
